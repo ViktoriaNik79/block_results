911d8738fcde412faff84a18ef1990377f54a778
Виктория Коснырева <viktoria-nik79@bk.ru>
2022-09-20 20:13:01 +0300

    task_C#

---
+++ task_C#/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Задача: Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+длина которых меньше либо равна трём символам. Первоначальный массив можно ввести с клавиатуры,
+либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+лучше обойтись исключительно массивами.
+На примере: ["'hello", "2", "world", " :-)"] -> ["2", ":-)"] */
+
+string[] inpArr = {"Задан массив:  hello", "2", "world", ":-)"};
+Console.WriteLine($"{String.Join(", ", inpArr)}"); 
+string[] endArr = DesArr(inpArr);
+Console.Write($"Вывод c символами < или = трём:  [{String.Join(", ", endArr)}]");
+
+string[] DesArr(string[] inp) {string[] end = new string[]{}; int j = 0;
+for (int i = 0; i < inp.Length; i++)
+if (inp[i].Length <= 3) {Array.Resize(ref end, end.Length + 1); end[j] = inp[i]; j++;}
+return end;}  
